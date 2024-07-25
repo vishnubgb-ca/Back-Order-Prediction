6bd7160d70c412e0666175c892e48a7a71f147e9
Vishnu_B_G_Bharadwaja <146937608+vishnubgb-ca@users.noreply.github.com>
2024-07-25 16:04:40 +0530

    Update trigger_airflow_dag.py

--- trigger_airflow_dag.py
+++ trigger_airflow_dag.py
@@ -6,7 +6,7 @@ deployment_url = os.environ["airflow_url"]
 print(deployment_url)
 print(os.environ['airflow_username'])
 print(os.environ['airflow_password'])
-dag_id = "Back-Order-Prediction_Vishnu B G Bharadwaja"
+dag_id = "Back-Order-Prediction_VishnuB G Bharadwaja"
 # sleep(5)
 response = requests.post(
     url=f"{deployment_url}/api/v1/dags/{dag_id}/dagRuns",
